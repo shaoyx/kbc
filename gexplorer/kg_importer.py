d85b6055a0c02e01e83f75f2fdd7e98277eaf66a
yxshao <simon0227@gmail.com>
2018-04-01 20:56:43 +0800

    add gexplorer code

---
+++ gexplorer/kg_importer.py
@@ -0,0 +1,23 @@
+from neo4j.v1 import GraphDatabase
+
+driver = GraphDatabase.driver("bolt://localhost:7688", auth=("neo4j", "123456"))
+
+kg_path_train = "../KBEcodes/dat/wordnet-mlj12/wordnet-mlj12-train.txt"
+# kg_path_train = "../KBEcodes/dat/wordnet-mlj12/test"
+kg_path_train = "../KBEcodes/dat/FB15k/freebase_mtr100_mte100-train.txt"
+
+def add_relation(tx, head, rel, tail):
+    tx.run("MERGE (h:Word {name: $head})"
+           "MERGE (t:Word {name: $tail})"
+           "MERGE (h)-[:Type {name: $rel}]->(t)",
+           head=head, rel=rel, tail=tail)
+
+with driver.session() as session:
+    with open(kg_path_train, "r") as kg_data:
+        cnt = 0;
+        for line in kg_data:
+            head, rel, tail = line.strip().split('\t')
+            cnt = cnt + 1
+            if cnt % 10000 == 0:
+                print(cnt)
+            session.write_transaction(add_relation, head, rel, tail)
